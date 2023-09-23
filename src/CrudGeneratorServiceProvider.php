64514475918cdfac8a923f4c3593926cf1365d6a
ferasalhallaknamaa2 <f.alhallak2@namaa-solutions.com>
2023-09-23 16:05:00 +0300

    crud-generator

---
+++ src/CrudGeneratorServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Namaa\Namaa;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * Used to initialize some routes or add an event listener
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * Used to bind our package to the classes inside the app container
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
